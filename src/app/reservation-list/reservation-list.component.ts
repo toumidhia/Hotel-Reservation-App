80aad41d5918cd8ee158f6d79ea4aab392e41131
toumidhia <138765744+toumidhia@users.noreply.github.com>
2024-08-06 10:04:48 +0100

    Add files via upload

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -13,9 +13,13 @@ export class ReservationListComponent implements OnInit {
   constructor(private reservationService: ReservationService){}
   
   ngOnInit(): void {
-    this.reservations = this.reservationService.getReservations();
+    this.reservationService.getReservations().subscribe(reservations => {
+      this.reservations = reservations;
+    });
   }
   deleteReservation(id: string){
-    this.reservationService.deleteReservation(id);
+    this.reservationService.deleteReservation(id).subscribe(() => {
+      console.log("Delete request get processed.")
+    })
   }
 }
